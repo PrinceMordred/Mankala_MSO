03e87bff62cdc939d04939278596964690c7aad4
SilasPeters <silaspeters03@gmail.com>
2022-10-10 15:56:00 +0200

    fix

---
+++ Mankala/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const int steentjesPerGat = 4;
+const int gatenPerSpeler  = 6;
+
+(byte, byte)[] eigenhokjes = new (byte, byte)[gatenPerSpeler];
+
+string thuisHok(int n) => $@"##
+##
+#{n}
+##
+##";
+string spelerHokjes(int u, int l) => $@"#
+{u}
+#
+{l}
+#";
+
+Console.WriteLine(
+	thuisHok(0)
+	+ eigenhokjes.Select(x => spelerHokjes(x.Item1, x.Item2))
+);
