eb9f12f75b507a98f0b2db70db40405557e2da77
Silas Peters <silaspeters03@gmail.com>
2022-10-30 13:10:38 +0100

    Input is now done

--- Mankala/Board.cs
+++ Mankala/Board.cs
@@ -7,14 +7,27 @@ public class Board
 {
 	private readonly byte _stonesPerHole;
 	private readonly byte _holesPerPlayer;
-	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	
 	private byte[] _holes; // n holes for p1, 1 base hole for p1, n holes for p2, 1 base hole for p2
 
-	public byte   GetBaseP1  => _holes[_holesPerPlayer];
-	public byte   GetBaseP2  => _holes.Last();
-	public byte[] GetHolesP1 => _holes[.._holesPerPlayer];
-	public byte[] GetHolesP2 => _holes[(_holesPerPlayer + 1) .. ^1];
+	public int IndexOfBaseHoleP1 => _holesPerPlayer;
+	public int IndexOfBaseHoleP2 => _holes.Length - 1;
+	public Range RangeOfHolesP1 => new(0, IndexOfBaseHoleP1);
+	public Range RangeOfHolesP2 => new(IndexOfBaseHoleP1 + 1, IndexOfBaseHoleP2);
 	
+	public byte   GetBaseP1  => _holes[IndexOfBaseHoleP1];
+	public byte   GetBaseP2  => _holes[IndexOfBaseHoleP2];
+	public byte[] GetHolesP1 => _holes[RangeOfHolesP1];
+	public byte[] GetHolesP2 => _holes[RangeOfHolesP2];
+
+	private bool _IsBaseHoleOf(int player, int i) => player switch
+	{
+		1 => i == _holesPerPlayer,
+		2 => i == _holes.Length - 1,
+		_ => throw new ArgumentOutOfRangeException(
+			"A player was referenced with an incorrect index, which should be 1 or 2: " + player.ToString())
+	};
+
 	public Board(byte stonesPerHole, byte holesPerPlayer)
 	{
 		_stonesPerHole = stonesPerHole;
@@ -29,7 +42,8 @@ public class Board
 		Array.Fill(_holes, _stonesPerHole, _holesPerPlayer + 1, _holesPerPlayer);
 	}
 
-	public void Print()
+	/// <returns>A tuple containing: board ASCII art, width of the art, width of a single hole, offset of the first player hole</returns>
+	public (string, int, int, int) PrintBoard()
 	{
 		// Calculate widths to base printing on
 		var holeDisplayWidth = _holes.Max().ToString().Length + 2;
@@ -38,9 +52,9 @@ public class Board
 
 		// Print
 		printFilledLine();
+		printHolesLine(GetHolesP2.Reverse());
+		printBaseHoleLine(GetBaseP2, GetBaseP1);
 		printHolesLine(GetHolesP1);
-		printBaseHoleLine(GetBaseP1, GetBaseP2);
-		printHolesLine(GetHolesP2);
 		printFilledLine();
 
 		void printFilledLine()
@@ -68,8 +82,34 @@ public class Board
 
 		string padHole(byte hole) => hole.ToString().PadLeft(holeDisplayWidth - 1, ' ') + ' ';
 
-		Console.WriteLine(sb.ToString());
+		return (sb.ToString(), width, holeDisplayWidth, 2);
 	}
 
-	public IBoardState GetBoardState() => new MankalaBoardState(GetBaseP1, GetBaseP2, GetHolesP1, GetHolesP2);
+	public IBoardState GetBoardState() =>
+		new MankalaBoardState(GetBaseP1, GetBaseP2, GetHolesP1, GetHolesP2);
+
+	/// <param name="holeIndex">the hole from which the move is performed, AKA the hole which stones are spread</param>
+	/// <param name="otherPlayerIndex">The index of the OTHER player</param>
+	/// <returns>The new state after performing the move</returns>
+	public IBoardState MakeMove(int holeIndex, int otherPlayerIndex)
+	{
+		// pick up all stones
+		var stonesToSpread = _holes[holeIndex];
+		_holes[holeIndex] = 0;
+		
+		// spread stones
+		while (stonesToSpread > 0)
+		{
+			var currentHole = ++holeIndex % _holes.Length;
+			
+			if (_IsBaseHoleOf(otherPlayerIndex, currentHole))
+				continue; // Don't move over opponents base hole TODO: make this dependent on IRuleSet
+			
+			_holes[currentHole] += 1;
+			stonesToSpread  -= 1;
+		}
+		
+		// return new state
+		return GetBoardState();
+	}
 }
--- Mankala/Program.cs
+++ Mankala/Program.cs
@@ -1,8 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 using Mankala;
 
-const byte startingStonesPerHole = 42;
-const byte holesPerPlayer = 9;
+const byte startingStonesPerHole = 4;
+const byte holesPerPlayer = 3;
 
 Board board = new(startingStonesPerHole, holesPerPlayer);
-board.Print();
+
+// remember the state of who (and how) selects the next hole to make a move
+var playerOneIsOn = true;
+int selectedHoleP1 = 0;
+int selectedHoleP2 = board.IndexOfBaseHoleP2 - 1;
+
+while (true)
+{
+	// interpret current state
+	ref var currentHoleIndex = ref playerOneIsOn ? ref selectedHoleP1 : ref selectedHoleP2;
+	var rangeOfHolesOfCurrentPlayer = playerOneIsOn ? board.RangeOfHolesP1 : board.RangeOfHolesP2;
+	var otherPlayerIndex = playerOneIsOn ? 2 : 1; 
+	
+	// Ask the player to make a move
+	Console.Clear();
+	selectHole(ref currentHoleIndex, rangeOfHolesOfCurrentPlayer);
+	Console.Beep();
+	
+	// Make instructed move
+	board.MakeMove(currentHoleIndex, otherPlayerIndex);
+	playerOneIsOn = !playerOneIsOn;
+}
+
+// Converts selectedHoleOfCurrentPlayer to the new selected hole index
+void selectHole(ref int currentHoleIndex, Range holeIndexes)
+{
+	// Print the board and the selection arrow to the console.
+	var (holesOffset, holeWidth, arrowY) = printBoardAndSelectionArrow(!playerOneIsOn);
+	var currentArrowX = 0; // Note that the selection arrow is printed on x=0 by default
+	
+	moveSelectionArrow(currentHoleIndex); // Move the arrow to the standard/last known selected hole
+
+	ConsoleKey inputKey;
+	while ((inputKey = Console.ReadKey().Key) != ConsoleKey.Enter) // Keep moving the selection arrow until player presses enter
+	{
+		switch (inputKey)
+		{
+			case ConsoleKey.RightArrow: currentHoleIndex += playerOneIsOn ? 1 : -1; // the ternary compensates for the fact that the upper holes go from right to left
+				break;
+			case ConsoleKey.LeftArrow:  currentHoleIndex += playerOneIsOn ? -1 : 1; // the ternary compensates for the fact that the upper holes go from right to left
+				break;
+			default: // wrong keypresses should be ignored
+				continue;
+		}
+		// Clamp selected hole to player's holes
+		currentHoleIndex = Math.Clamp(currentHoleIndex, holeIndexes.Start.Value, holeIndexes.End.Value - 1);
+		
+		// Reset cursor position after input
+		var (x, y) = Console.GetCursorPosition();
+		Console.SetCursorPosition(x -1, y);
+
+		// Move the arrow to the new selected hole
+		moveSelectionArrow(currentHoleIndex);
+	}
+
+	// Literally moves the arrow in the console to the right hole
+	void moveSelectionArrow(int currentHoleIndex)
+	{
+		// Make the arrow go form right to left when selecting upper row
+		var nthHoleFromLeft = playerOneIsOn ? currentHoleIndex : (holeIndexes.End.Value - 1) - currentHoleIndex; 
+		// Determine (based on the above trick) the new offset of the arrow
+		var newArrowX = holesOffset + nthHoleFromLeft * holeWidth + holeWidth - 2; // offset + x of hole + right side of hole
+		
+		Console.MoveBufferArea( //todo: MoveBufferArea only works in windows...
+			// source x, y, width, height
+			currentArrowX, arrowY, 1, 2, 
+			// target x, y
+			newArrowX, arrowY);
+
+		currentArrowX = newArrowX;
+	}
+}
+
+(int, int, int) printBoardAndSelectionArrow(bool selectUpperHole)
+{
+	var (boardAscii, boardWidth, holeWidth, holesOffset) = board.PrintBoard();
+	StringBuilder stringBuilder = new();
+	
+	// create potential arrow above the board
+	stringBuilder.Append(selectUpperHole ? "|\nV\n" : "\n\n");
+	
+	// create the board
+	stringBuilder.Append(boardAscii);
+	
+	// create potential arrow under the board
+	stringBuilder.Append(!selectUpperHole ? "^\n|\n" : "\n\n");
+	
+	// print to console
+	Console.WriteLine(stringBuilder);
+
+	var arrowY = selectUpperHole ? 0 : 2 + 5; // padding + board height
+	return (holesOffset, holeWidth, arrowY);
+}
