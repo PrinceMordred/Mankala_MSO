12a5856a64661e021e5825e5b6050ae4e2a94c75
Prince Mordred <jackrosenberg03@gmail.com>
2022-11-04 12:00:23 +0100

    [ADD] classes player and IRuleset

---
+++ Mankala/MankalaRuleset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mankala
+{
+    internal class MankalaRuleset : IRuleset
+    {
+        public int numHolesPerPlayer() => 6;
+        public int numStartStones()    => 4;
+        public bool CheckValidMove(IBoardState state, int clickedHole)
+        {
+            if (state.HolesP1()[clickedHole] == null)
+        }
+
+        public bool CheckWin(IBoardState state)
+        {
+            IEnumerable<byte> s = state.HolesP1();
+            foreach (byte b in s)
+                if (b != 0)
+                    return false;
+            return true;
+        }
+        public Player NextPlayer(Player p, IBoardState state)
+        {
+            return p;
+        }
+    }
+}
