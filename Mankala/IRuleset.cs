6680522273fc173b750d36a8f0007bd6cec5d5a6
Prince Mordred <jackrosenberg03@gmail.com>
2022-11-04 12:00:23 +0100

    [ADD] classes player and IRuleset

---
+++ Mankala/IRuleset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mankala
+{
+    public interface IRuleset
+    {
+        // variables for a Ruleset
+        public int numHolesPerPlayer();
+        public int numStartStones();
+
+        // methods for a ruleset
+        public bool CheckValidMove(IBoardState state, int clickedHole);
+        public bool CheckWin      (IBoardState state);
+        public int NextPlayer     (IBoardState state);
+    }
+}
---
+++ Mankala/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mankala
+{
+    public class Player
+    {
+        public ConsoleColor cColor;
+        public int Score;
+        public bool IsAi;
+
+        public Player(ConsoleColor _cColor, int _score, bool _isAi)
+        {
+            cColor = _cColor;
+            Score = _score;
+            IsAi = _isAi;
+        }
+    }
+}
