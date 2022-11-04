79524a47d21c445b5b3cbd982df1c3d5a94c0694
Silas Peters <silaspeters03@gmail.com>
2022-11-04 11:58:48 +0100

    [REFACTOR BoardState is now gone

--- Mankala/IBoardState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mankala;
-
-public interface IBoardState
-{
-	public byte BaseHoleP1();
-	public byte BaseHoleP2();
-	public IEnumerable<byte> HolesP1();
-	public IEnumerable<byte> HolesP2();
-}
