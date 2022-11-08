c2725ba23201c8dc7e87507a898e91f5d293ca69
PrinceMordred <jackrosenberg03@gmail.com>
2022-11-08 21:47:22 +0100

    latest

--- Mankala/GameLogics/MankalaGameLogic.cs
+++ Mankala/GameLogics/MankalaGameLogic.cs
@@ -1,32 +1,38 @@
 ﻿using Mankala.GameLogics;
+using Mankala.Rulesets;
 
 namespace Mankala;
 
-public class MankalaGameLogic
+public class MankalaGameLogic : GameLogic
 {
-	public class MankalaGameLogic : GameLogic
-	{
-		public MankalaGameLogic(Board board) : base(board)
-		{
-		}
+    public MankalaGameLogic(Board board, IRuleset ruleset) : base(board, ruleset)
+    {
+    }
 
-		public override void MakeMove(int holeIndex, int otherPlayerIndex)
-		{
-			// pick up all stones
-			var stonesToSpread = _holes[holeIndex];
-			_holes[holeIndex] = 0;
-		
-			// spread stones
-			while (stonesToSpread > 0)
-			{
-				var currentHole = ++holeIndex % _holes.Length;
-			
-				if (_IsBaseHoleOf(otherPlayerIndex, currentHole))
-					continue; // Don't move over opponents base hole TODO: make this dependent on IRuleSet
-			
-				_holes[currentHole] += 1;
-				stonesToSpread      -= 1;
-			}
-		}
+    public override int MakeMove(int holeIndex, int otherPlayerIndex)
+	{
+        int finalHoleIndex = 0;
+        // pick up all stones out of a hole
+        byte stonesToSpread = _board.GetHoles[holeIndex];
+		_board.GetHoles[holeIndex] = 0;
+        Board.HoleKind oppositeHoleIndex = otherPlayerIndex == 1 ? Board.HoleKind.MainHoleP1 : Board.HoleKind.MainHoleP2;
+        // spread stones
+        while (stonesToSpread > 0)
+        {
+            // move to next hole
+            HoleCycle.MoveNext();
+            // if the hole is not the other player's base hole, put a stone in it
+            if (HoleCycle.Current.Item2 == oppositeHoleIndex)
+                continue;
+            else
+            {
+                _board.GetHoles[HoleCycle.Current.Item1]++;
+                stonesToSpread--;
+                if (stonesToSpread == 0)
+                    finalHoleIndex = HoleCycle.Current.Item1;
+            }
+        }
+        return finalHoleIndex;
 	}
+	
 }
