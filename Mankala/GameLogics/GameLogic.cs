82f7976b3762a46013e5de4377c47bfbe8968e61
Prince Mordred <jackrosenberg03@gmail.com>
2022-11-09 14:47:35 +0100

    help

--- Mankala/GameLogics/GameLogic.cs
+++ Mankala/GameLogics/GameLogic.cs
@@ -16,7 +16,7 @@ public abstract class GameLogic
 	// factory methods
 	protected virtual bool    CheckValidMove(Player player, int selectedHole)
 	{
-        return !(_board.GetHoles[selectedHole] == 0);
+        
     }
 	public abstract    Player? GetWinner();
 	protected abstract Player  NextPlayer(Player player, int lastHoleIndex);
@@ -32,36 +32,11 @@ public abstract class GameLogic
 	/// <returns> The player who is up next </returns>
 	public virtual (Player, int) MakeMove(Player player, int holeIndex) // Template method //returns the player who played and the index of the last hole
 	{
-		if (!CheckValidMove(player, holeIndex))
-			return (player, -1);
-
-		return (NextPlayer(player, PerformMove(holeIndex)), -1);
+		
 	}
 	
 	public int PerformMove(Player player, int holeIndex)
 	{
-		int finalHoleIndex = 0;
-		// pick up all stones out of a hole
-		byte stonesToSpread = _board.GetHoles[holeIndex];
-		_board.GetHoles[holeIndex] = 0;
 		
-		// spread stones
-		var holeCycle = _board.GetHolesCycle();
-		while (stonesToSpread > 0)
-		{
-			// move to next hole
-			holeCycle.MoveNext();
-			// if the hole is not the other player's base hole, put a stone in it
-			if (holeCycle.Current.HoleKind is Board.HoleKind.MainHoleP2)
-				continue;
-			else
-			{
-				_board.GetHolesCopy[holeCycle.Current.StoneCount]++;
-				stonesToSpread--;
-				if (stonesToSpread == 0)
-					finalHoleIndex = holeCycle.Current.StoneCount;
-			}
-		}
-		return finalHoleIndex;
 	}
 }
--- Mankala/GameLogics/MankalaGameLogic.cs
+++ Mankala/GameLogics/MankalaGameLogic.cs
@@ -2,7 +2,7 @@
 
 public class MankalaGameLogic : GameLogic
 {
-	public MankalaGameLogic(Board board) : base(board)
+	public MankalaGameLogic(Board board, Player player) : base(board, player)
 	{
 	}
 
@@ -19,18 +19,6 @@ public class MankalaGameLogic : GameLogic
 
 	protected override Player NextPlayer(Player player, int lastHoleIndex)
 	{
-        // check if the lastHoleIndex is on the playes side
-        if (player.PlayerNumber == 1)
-        {
-            if (lastHoleIndex <= _board.IndexOfBaseHoleP1)
-                return player;
-            return new Player(2, "" ,ConsoleColor.Red, 0);
-        }
-        else
-        {
-            if (lastHoleIndex > _board.IndexOfBaseHoleP1 && lastHoleIndex <= _board.IndexOfBaseHoleP2)
-                return player;
-            return new Player(1, "",  ConsoleColor.Blue, 0);
-        }
+        //TODO
     }
 }
