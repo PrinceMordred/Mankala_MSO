2b38e7e3e87921d88f37b3894150c11e37a7ae2c
Prince Mordred <jackrosenberg03@gmail.com>
2022-11-09 12:49:28 +0100

    generated constructor and methods

--- Mankala/GameLogics/WariGameLogic.cs
+++ Mankala/GameLogics/WariGameLogic.cs
@@ -2,5 +2,22 @@
 
 public class WariGameLogic : GameLogic
 {
-	
+	public WariGameLogic(Board board) : base(board)
+	{
+	}
+
+	public override Player? GetWinner()
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override bool CheckValidMove(int selectedHole)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override Player NextPlayer(Player player)
+	{
+		throw new NotImplementedException();
+	}
 }
