d39687513500fed0fe78941fa4a4b92aed938242
PrinceMordred <jackrosenberg03@gmail.com>
2022-11-11 20:08:14 +0100

    froyo

--- Mankala/GameLogics/FrozenYoghurtGameLogic.cs
+++ Mankala/GameLogics/FrozenYoghurtGameLogic.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Mankala.GameLogics
 {
-    internal class FrozenYoghurtGameLogic
+    internal class FrozenYoghurtGameLogic : GameLogic
     {
+        public FrozenYoghurtGameLogic(Board board, Player[] pList) : base(board, pList)
+        {
+        }
+
+        public override int PerformMove(Player player, int holeIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Player NextPlayer(Player player, int lastHoleIndex)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
